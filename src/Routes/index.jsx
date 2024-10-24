a486f060e1fcd4f091777cd79fc020268d6acde0
Cezar rull <cezarrull12345@gmail.com>
2024-10-24 14:37:55 -0300

    :fire: ScrollToTop

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 
 import { AppRoutes } from "./app.routes";
+import ScrollToTop from "./ScrollToTop";
 
 export function Routes() {
     return(
         <BrowserRouter>
+            <ScrollToTop />
             <AppRoutes />
         </BrowserRouter>
     )
